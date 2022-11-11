a78466c719b893ee4da6e919d8a2bf251c740c94
ashley <benjamin.liu@live.cn>
2022-11-11 18:15:35 -0500

    _0083_e_Remove_Duplicates_from_Sorted_List

---
+++ questions/linklist/Q_0083_e_Remove_Duplicates_from_Sorted_List.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return head
+}
