f8a032049db03ed408176fed6ea00e05531dba2e
ashley <benjamin.liu@live.cn>
2022-11-16 19:07:06 -0500

    _0344_e_Reverse_String

---
+++ questions/array/Q_0344_e_Reverse_String.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+}
+
+func reverseString(s []byte) {
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		s[left], s[right] = s[right], s[left]
+	}
+}
