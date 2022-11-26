6bb6aaafb12516dce81a83f54c3bff2270c6459a
ashley <benjamin.liu@live.cn>
2022-11-25 19:40:14 -0500

    _0054_m_Spiral_Matrix

--- utils/PrintUtil.go
+++ utils/PrintUtil.go
@@ -10,8 +10,16 @@ func PrintListNode(head *structures.ListNode) {
 		fmt.Printf("%d ->", head.Val)
 		head = head.Next
 	}
+	fmt.Println()
 }
 
 func Println(num int) {
 	fmt.Println(num)
+}
+
+func PrintList(list []int) {
+	for _, val := range list {
+		fmt.Printf("%d ->", val)
+	}
+	fmt.Println()
 }
