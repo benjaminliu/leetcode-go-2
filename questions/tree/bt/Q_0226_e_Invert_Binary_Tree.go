62dfb2d0e1b5a00b87e0646ea7103e3f3f7fdc1e
ashley <benjamin.liu@live.cn>
2022-12-08 19:15:13 -0500

    _0226_e_Invert_Binary_Tree

---
+++ questions/tree/bt/Q_0226_e_Invert_Binary_Tree.go
@@ -0,0 +1,13 @@
+package main
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+
+	root.Left, root.Right = right, left
+	return root
+}
